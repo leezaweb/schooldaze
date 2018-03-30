003e86414ceb6b43790e1512b2a08ece3604a7d1
azeelleeza <leezaluncheon@gmail.com>
2018-03-30 11:07:08 -0400

    cli qt

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,7 @@ require 'bundler'
 Bundler.require
 require 'require_all'
 require 'table_print'
+require 'colorize'
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
 require_all 'lib'
--- lib/CommandLineInterface.rb
+++ lib/CommandLineInterface.rb
@@ -1,19 +1,25 @@
-
+BLUE='\033[1;34m'
+NC='\033[0m'
 
 def greet
-  puts "Welcome to School, the command line solution to registering for classes and other misc. questions you might have!"
+summary = "\n\nWelcome to School Days, the command line
+solution to registering for classes and
+other miscellaneous questions you might have!
+
+"
+
+puts summary.lines.map { |line| line.center(82).light_blue.bold }
 end
 
 def interactive
-  puts "\nPlease select one of the following:"
+  puts "Please select one of the following:".center(82).magenta.bold
 
-  puts "\n1 - Student\n2 - Teacher\n3 - Exit"
 
+  puts "\n\t\t\t\t1 - Student\n\t\t\t\t2 - Teacher\n\t\t\t\t3 - Exit".magenta.bold
   user_selection = gets.chomp
   system('clear')
 
   if (user_selection == "")
-    puts "You must make a selection."
     interactive
   elsif user_selection == '1'
     puts "_____________________________________"
@@ -22,11 +28,8 @@ def interactive
     puts "_____________________________________"
     teacher_method
   elsif user_selection == '3'
-    puts "Don't be a fool, stay in school! Have a nice day!"
+    puts "Have a nice day!"
     exit
-  else
-    puts "Invalid selection!"
-    interactive
   end
 
 
