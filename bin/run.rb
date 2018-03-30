6dcc5d28b2a19ace72d6f86eb5a28e013e8a3566
github username <github email address>
2018-03-30 11:51:29 -0400

    clears first screen

--- bin/run.rb
+++ bin/run.rb
@@ -1,6 +1,6 @@
 require_relative '../config/environment.rb'
 
-
+system('clear')
 puts "                                                                                               .
            .--.--.               ,---,                          ,--,                           .
           /  /    '.           ,--.' |                        ,--.'|                           .
